b6f52111817e5b4cf7ff6fb319d2f962fe6cb7e6
tsrinarmwong <tsrinarmwong@hawk.iit.edu>
2024-01-23 15:30:49 -0600

    Leetcode Day 5 #1

---
+++ 11_Container_With_Most_Water.py
@@ -0,0 +1,43 @@
+'''
+11. Container With Most Water
+
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+'''
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0  # Start with the leftmost wall
+        right = len(height) - 1  # Start with the rightmost wall
+        max_area = 0  # Initialize max area as zero
+
+        while left < right:  # Continue until the two pointers meet
+            # Calculate the area with the current pair of walls
+            current_area = (right - left) * min(height[left], height[right])
+            max_area = max(max_area, current_area)  # Update max area if the current area is larger
+
+            # Move the pointer of the shorter wall inward
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area  # Return the maximum area found
