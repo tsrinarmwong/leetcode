ebf4760db4e0b3ddd06a53234c6bbd22978a513d
Timo S. <125150205+tsrinarmwong@users.noreply.github.com>
2026-01-06 11:10:52 -0500

    Add maxLevelSum function for binary tree level sums
    Implement maxLevelSum to find the maximum level sum in a binary tree.

---
+++ 1161 Maximum Level Sum of a Binary Tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        q = collections.deque()
+        q.append(root)
+
+        level = 1
+        max_sum = float("-inf")
+        best_level = 1
+
+        while q:
+            level_sum = 0
+
+            for _ in range (len(q)):
+                node = q.popleft()
+                level_sum += node.val
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            if level_sum > max_sum:
+                max_sum = level_sum
+                best_level = level
+            
+            level += 1
+
+        return best_level
